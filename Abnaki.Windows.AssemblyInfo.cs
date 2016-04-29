819e94686d5f1032cfa597fb323e295d1191992a
abnaki <abnaki>
2016-04-29 17:55:04 -0400

    Rafactored into Abnaki.Windows.AssemblyInfo.cs

---
+++ Abnaki.Windows.AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿// Essential commonalities of all assemblies
+
+using System.Reflection;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Abnaki Way Components")]
+[assembly: AssemblyCopyright("Copyright © Abnaki Way Components 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("0.9.0.0")]
+[assembly: AssemblyFileVersion("0.9.0.0")]
