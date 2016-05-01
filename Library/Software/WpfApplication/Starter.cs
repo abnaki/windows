0f4ff4989b1aa0c7fb4f2db6a904edaf4ee3280f
abnaki <abnaki>
2016-05-01 13:10:55 -0400

    $1

--- Library/Software/WpfApplication/Starter.cs
+++ Library/Software/WpfApplication/Starter.cs
@@ -1,14 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Windows;
-using System.Diagnostics;
 
 namespace Abnaki.Windows.Software.Wpf
 {
+    /// <summary>
+    /// Starts Application with suitable preliminaries
+    /// </summary>
     public class Starter
     {
-        [STAThread]
-        public static int Main<Tapp, Twin>(string[] args)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="Tapp"></typeparam>
+        /// <typeparam name="Twin"></typeparam>
+        /// <param name="args"></param>
+        /// <example>
+        /// [STAThread]
+        /// public static int Main(string[] args)
+        /// {  return Starter.Start&lt;YourApp,Yourwin>(args); }
+        /// </example>
+        public static int Start<Tapp, Twin>(string[] args)
             where Tapp : System.Windows.Application, new()
             where Twin : System.Windows.Window, new()
         {
