4c5f73406c22d335950d840225115c3280c0fb3f
abnaki <abnaki>
2016-05-01 13:10:55 -0400

    $1

---
+++ Examples/WPF/Ex01_Hello/ExStarter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex01_Hello
+{
+    class ExStarter
+    {
+        /// <summary>
+        /// Note that the project property Application...Startup object is set to this class.
+        /// </summary>
+        [STAThread]
+        public static int Main(string[] args)
+        {
+            return Abnaki.Windows.Software.Wpf.Starter.Start<App, MainWindow>(args);
+
+        }
+    }
+}
