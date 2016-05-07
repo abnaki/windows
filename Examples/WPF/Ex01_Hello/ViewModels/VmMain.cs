c7111c8b5dbc512448c03b5abcef709c1cc3b4e2
abnaki <abnaki>
2016-05-07 16:52:47 -0400

    $1

---
+++ Examples/WPF/Ex01_Hello/ViewModels/VmMain.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Input;
+
+namespace Ex01_Hello.ViewModels
+{
+    class VmMain : Abnaki.Windows.Software.Wpf.AbnakiViewModel
+    {
+        public VmMain()
+        {
+            CmdThrowException = new Commands.ThrowException();
+            CmdSaveLog = new Commands.SaveLog();
+        }
+
+        // public because internal causes inactive buttons
+        public ICommand CmdThrowException { get; private set; }
+        public ICommand CmdSaveLog { get; private set; }
+
+    }
+}
