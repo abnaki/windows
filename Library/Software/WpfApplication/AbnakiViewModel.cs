8b5999ea5d48f3d4fb6b0a5c578be80c4fd93e22
abnaki <abnaki>
2016-05-07 16:52:47 -0400

    $1

---
+++ Library/Software/WpfApplication/AbnakiViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel;
+
+namespace Abnaki.Windows.Software.Wpf
+{
+    /// <summary>
+    /// Base class
+    /// </summary>
+    public class AbnakiViewModel : INotifyPropertyChanged
+    {
+        event PropertyChangedEventHandler INotifyPropertyChanged.PropertyChanged
+        {
+            add
+            {
+                //throw new NotImplementedException();
+            }
+            remove
+            {
+                //throw new NotImplementedException();
+            }
+        }
+    }
+}
