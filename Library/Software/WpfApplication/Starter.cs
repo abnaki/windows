c9fa47fdb5f12b398aa6eb19064ce50a6c835cd0
abnaki <abnaki>
2016-05-07 16:52:47 -0400

    $1

--- Library/Software/WpfApplication/Starter.cs
+++ Library/Software/WpfApplication/Starter.cs
@@ -11,19 +11,21 @@ namespace Abnaki.Windows.Software.Wpf
     public class Starter
     {
         /// <summary>
-        /// 
+        /// Starts Application having Twindow as first thing user sees.
         /// </summary>
-        /// <typeparam name="Tapp"></typeparam>
-        /// <typeparam name="Twin"></typeparam>
+        /// <typeparam name="Tapp">System.Windows.Application, or subclass if necessary</typeparam>
+        /// <typeparam name="Twindow">class of window</typeparam>
         /// <param name="args"></param>
         /// <example>
+        /// <code>
         /// [STAThread]
         /// public static int Main(string[] args)
-        /// {  return Starter.Start&lt;YourApp,Yourwin>(args); }
+        /// {  return Starter.Start<YourApp,YourWin>(args); }
+        /// </code>
         /// </example>
-        public static int Start<Tapp, Twin>(string[] args)
+        public static int Start<Tapp, Twindow>(string[] args)
             where Tapp : System.Windows.Application, new()
-            where Twin : System.Windows.Window, new()
+            where Twindow : System.Windows.Window, new()
         {
             //Process proc = System.Diagnostics.Process.GetCurrentProcess();
             //ProcessStartInfo pistart = proc.StartInfo; // no use
@@ -31,12 +33,13 @@ namespace Abnaki.Windows.Software.Wpf
             var starta = System.Reflection.Assembly.GetEntryAssembly();
             Log.Comment("Start", starta.Location);
             Log.Comment("Version", starta.GetName().Version);
+            Log.Comment("Operating System", Environment.OSVersion);
 
             Tapp app = new Tapp();
 
             app.DispatcherUnhandledException += Current_DispatcherUnhandledException;
 
-            Twin mw = new Twin();
+            Twindow mw = new Twindow();
             return app.Run(mw);
         }
 
