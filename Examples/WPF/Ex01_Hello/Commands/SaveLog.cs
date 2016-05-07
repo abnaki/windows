db891f90edd87e133fa74031f11bedc767a47306
abnaki <abnaki>
2016-05-07 16:52:47 -0400

    $1

---
+++ Examples/WPF/Ex01_Hello/Commands/SaveLog.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Input;
+using SaveFileDialog = System.Windows.Forms.SaveFileDialog;
+
+namespace Ex01_Hello.Commands
+{
+    class SaveLog : ICommand
+    {
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            using (SaveFileDialog dialog = new SaveFileDialog())
+            {
+                dialog.Filter = "XML|.xml";
+                dialog.DefaultExt = ".xml";
+                dialog.Title = "Save Log File";
+
+                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    Abnaki.Windows.Log.Write(dialog.FileName);
+                }
+            }
+
+        }
+    }
+}
