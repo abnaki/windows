0124ff4ac742578f5edee94b437ec8c988c01be4
abnaki <abnaki>
2016-05-07 16:52:47 -0400

    $1

---
+++ Examples/WPF/Ex01_Hello/Commands/ThrowException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Ex01_Hello.Commands
+{
+    class ThrowException : ICommand
+    {
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            throw new ApplicationException("Demonstration Exception");
+        }
+    }
+}
