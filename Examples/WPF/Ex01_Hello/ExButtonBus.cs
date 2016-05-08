cd63c4524e1b001d4093675d30745ecd85673934
abnaki <abnaki>
2016-05-08 14:17:25 -0400

    Refactored into ExButtonBus and ButtonBus

---
+++ Examples/WPF/Ex01_Hello/ExButtonBus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Abnaki.Windows.Software.Wpf.Menu;
+
+
+namespace Ex01_Hello
+{
+    /// <summary>
+    /// Uses MessageTube with buttons
+    /// </summary>
+    class ExButtonBus : ButtonBus<ButtonKey>
+    {
+        protected override void HandleButton(ButtonMessage m)
+        {
+            base.HandleButton(m);
+            Debug.WriteLine(m);
+        }
+    }
+}
