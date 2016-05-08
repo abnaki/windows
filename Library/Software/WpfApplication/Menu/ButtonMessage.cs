d9c1bbbbdadcd305accaa6fbf56702dafe0c23ac
abnaki <abnaki>
2016-05-08 14:17:25 -0400

    $1

---
+++ Library/Software/WpfApplication/Menu/ButtonMessage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+
+namespace Abnaki.Windows.Software.Wpf.Menu
+{
+    /// <summary>
+    /// Supports messaging about buttons/commands clicked
+    /// </summary>
+    public class ButtonMessage
+    {
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="Tenum">contains keys of related buttons
+    /// </typeparam>
+    public class ButtonMessage<Tenum> : ButtonMessage
+    {
+        public ButtonMessage(Tenum key)
+        {
+            this.Key = key;
+        }
+
+        public ButtonMessage(string s)
+        {
+            this.Key = (Tenum)Enum.Parse(typeof(Tenum), s, ignoreCase: true);
+        }
+
+        public Tenum Key { get; private set; }
+
+        public override string ToString()
+        {
+            return base.ToString() + " " + Key;
+        }
+    }
+
+}
