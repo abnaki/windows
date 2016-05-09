cc51abe509e331a115ddef533be853b0233907ec
abnaki <abnaki>
2016-05-09 16:13:34 -0400

    $1

---
+++ Examples/WPF/Ex02_Menu/ExButtonBus.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Abnaki.Windows.Software.Wpf.Menu;
+
+namespace Ex02_Menu
+{
+    /// <summary>
+    /// Uses MessageTube with buttons
+    /// </summary>
+    class ExButtonBus : ButtonBus<SubMenuKey>
+    {
+        protected override void HandleButton(ButtonMessage<SubMenuKey> m)
+        {
+            base.HandleButton(m);
+
+            //Debug.WriteLine(m);
+
+            switch ( m.Key )
+            {
+                case SubMenuKey.Exit:
+                    System.Windows.Application.Current.Shutdown();  // crude
+                    break;
+            }
+        }
+    }
+}
