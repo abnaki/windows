8483f8b9fac281fee29153fc072537183e4ccdde
abnaki <abnaki>
2016-05-09 16:41:57 -0400

    $1

---
+++ Examples/WPF/Ex02_Menu/ExStarter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Ex02_Menu
+{
+    class ExStarter
+    {
+        // also see Ex01_Menu ExStarter
+        [STAThread]
+        public static int Main(string[] args)
+        {
+            return Abnaki.Windows.Software.Wpf.Starter.Start<MainWindow>(args);
+        }
+    }
+}
