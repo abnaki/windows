04c7ac47ed6732647111577fee92f01378780523
abnaki <abnaki>
2016-05-10 15:32:04 -0400

    +Ex03_Ultimate

---
+++ Examples/WPF/Ex03_Ultimate/ExControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ex03_Ultimate
+{
+    /// <summary>
+    /// Interaction logic for ExControl.xaml
+    /// </summary>
+    public partial class ExControl : UserControl
+    {
+        public ExControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Examples/WPF/Ex03_Ultimate/ExStarter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Abnaki.Windows.Software.Wpf.Ultimate;
+
+namespace Ex03_Ultimate
+{
+    class ExStarter
+    {
+        [STAThread]
+        public static int Main(string[] args)
+        {
+            return UltimateStarter<ExControl>.Start(args);
+        }
+
+    }
+}
