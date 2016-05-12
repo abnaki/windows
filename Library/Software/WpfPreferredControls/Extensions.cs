121a27537129825d072e203ed7f31af2f79f6beb
abnaki <abnaki>
2016-05-12 13:21:36 -0400

    Nothing yet

---
+++ Library/Software/WpfPreferredControls/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xceed.Wpf.AvalonDock;
+
+namespace Abnaki.Windows.Software.Wpf.PreferredControls
+{
+    public static class Extensions
+    {
+        // will extend DockingManager publicly
+    }
+}
