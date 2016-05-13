a0c91dde02b95df3b137543a3e504c4d06b51bea
abnaki <abnaki>
2016-05-13 10:01:06 -0400

    System.Diagnostics

--- Library/Software/WpfApplication/Ultimate/MainWindow.xaml.cs
+++ Library/Software/WpfApplication/Ultimate/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
 
 using Abnaki.Windows.Software.Wpf.Menu;
 
