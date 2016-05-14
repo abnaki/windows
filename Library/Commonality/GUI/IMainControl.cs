845941090b66c963e3940dd6a12733c5e0dbcb39
abnaki <abnaki>
2016-05-14 16:33:47 -0400

    +IMainControl and IWindow

---
+++ Library/Commonality/GUI/IMainControl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Abnaki.Windows.GUI
+{
+    /// <summary>
+    /// Supports general initialization
+    /// </summary>
+    public interface IMainControl
+    {
+        /// <summary>
+        /// Manages panels
+        /// </summary>
+        /// <remarks>
+        /// The intent of object is to keep fundamental code free from many obscure freeware types, 
+        /// avoiding cluttered references of projects.  The docking/paneling system is always by a third party.
+        /// </remarks>
+        object DockingSystem { get; }
+    }
+}
---
+++ Library/Commonality/GUI/IWindow.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace Abnaki.Windows.GUI
+{
+    /// <summary>
+    /// Supports general initialization
+    /// </summary>
+    public interface IWindow
+    {
+        /// <summary>
+        /// Raised when panel/docking layout should be saved to file
+        /// </summary>
+        event Action<FileInfo> SavingLayout;
+
+        /// <summary>
+        /// Raised when panel/docking layout should be restored from file
+        /// </summary>
+        event Action<FileInfo> RestoringLayout;
+    }
+}
