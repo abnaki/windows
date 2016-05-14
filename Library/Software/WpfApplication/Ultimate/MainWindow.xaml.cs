29c88d2f70a9cc27f34fac00d2eec03dca8075fe
abnaki <abnaki>
2016-05-14 12:44:27 -0400

    Write/reads Prefs for window location

--- Library/Software/WpfApplication/Ultimate/MainWindow.xaml.cs
+++ Library/Software/WpfApplication/Ultimate/MainWindow.xaml.cs
@@ -31,6 +31,8 @@ namespace Abnaki.Windows.Software.Wpf.Ultimate
         {
             InitializeComponent();
 
+            this.Loaded += MainWindow_Loaded;
+
             mainMenub = new MainMenuBus(this.TopMenu);
         }
 
@@ -39,6 +41,93 @@ namespace Abnaki.Windows.Software.Wpf.Ultimate
         public void SetMainControl(System.Windows.UIElement c)
         {
             this.ClientPanel.Children.Add(c);
+        }
+
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        {
+            // but in OnClosed(), note that RestoreBounds is Empty 
+
+            base.OnClosing(e);
+
+            try
+            {
+                SaveBounds();
+
+                // save dock layout file
+            }
+            catch (Exception ex)
+            {
+                Log.Exception(ex);
+            }
+        }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            bool changed = false;
+            try
+            {
+                // note, in OnInitialized(), WindowState does not necessarily maximize on proper screen, so handle Loaded.
+                changed = ReloadBounds();
+
+                // open dock layout file
+            }
+            catch (Exception ex)
+            {
+                Diplomat.Notifier.Error(ex);
+            }
+            finally
+            {
+                if (changed) // previously hidden
+                {
+                    // this.Visibility = System.Windows.Visibility.Visible; // ineffective
+                    Show();
+                }
+            }
+        }
+
+        void SaveBounds()
+        {
+            Prefs pref = new Prefs();
+            if (this.RestoreBounds.IsEmpty)
+                pref.Bounds = new Rect(Left, Top, Width, Height);
+            else
+                pref.Bounds = this.RestoreBounds;
+
+            pref.State = this.WindowState;
+            Abnaki.Windows.Software.Wpf.Profile.Preference.WriteClassPrefs<MainWindow, Prefs>(pref);
+        }
+
+        bool ReloadBounds()
+        {
+            bool changed = false;
+
+            Prefs pref = Abnaki.Windows.Software.Wpf.Profile.Preference.ReadClassPrefs<MainWindow, Prefs>();
+            if (pref != null)
+            {
+                if (WpfScreenHelper.Screen.AllScreens.Any(sc => sc.WorkingArea.IntersectsWith(pref.Bounds)))
+                {
+                    this.Hide(); // show later
+                    Top = pref.Bounds.Top;
+                    Left = pref.Bounds.Left;
+                    Width = pref.Bounds.Width;
+                    Height = pref.Bounds.Height;
+
+                    this.WindowState = pref.State;
+                    changed = true;
+                }
+                else
+                {
+                    Log.Comment(GetType().Name + " disregarded undisplayable bounds " + pref.Bounds);
+                }
+            }
+
+            return changed;
+        }
+
+        public class Prefs
+        {
+            public Rect Bounds { get; set; }
+            public WindowState State { get; set; }
         }
     }
 }
