9bae3a8767c4839c972602de2a999e32ddc29209
abnaki <abnaki>
2016-05-17 12:28:18 -0400

    Diag/Design.cs

---
+++ Library/Software/WpfApplication/Diag/Design.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Abnaki.Windows.Software.Wpf.Diag
+{
+    /// <summary>
+    /// Investigate design of WPF controls
+    /// </summary>
+    public class Design
+    {
+        public static void DebugAncestry(DependencyObject x)
+        {
+            Debug.WriteLine("Ancestry:");
+            SubDebugAncestry(x);
+            Debug.IndentLevel = 0;
+        }
+
+        static void SubDebugAncestry(DependencyObject x)
+        {
+            if (x == null)
+                return;
+
+            DependencyObject parent = LogicalTreeHelper.GetParent(x);
+            SubDebugAncestry(parent);
+
+            Debug.Indent();
+            Debug.WriteLine(x);
+        }
+
+        // no effect
+        static void GetDescendants(DependencyObject x)
+        {
+            var children = LogicalTreeHelper.GetChildren(x).OfType<DependencyObject>();
+        }
+    }
+}
