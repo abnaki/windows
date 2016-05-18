c223e2f676608ab73e8e8d0cdde364f236214579
abnaki <abnaki>
2016-05-18 12:26:17 -0400

    Uses IDockSystem

--- Library/Commonality/GUI/IMainControl.cs
+++ Library/Commonality/GUI/IMainControl.cs
@@ -11,10 +11,6 @@ namespace Abnaki.Windows.GUI
         /// <summary>
         /// Manages panels
         /// </summary>
-        /// <remarks>
-        /// The intent of object is to keep fundamental code free from many obscure freeware types, 
-        /// avoiding cluttered references of projects.  The docking/paneling system is always by a third party.
-        /// </remarks>
-        object DockingSystem { get; }
+        IDockSystem DockingSystem { get; }
     }
 }
