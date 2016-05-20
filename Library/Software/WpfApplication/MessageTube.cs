aea586a714e90754338cf8666701f876c1b35451
abnaki <abnaki>
2016-05-20 17:51:48 -0400

    Subscribe<Tsuper,Tsub>

--- Library/Software/WpfApplication/MessageTube.cs
+++ Library/Software/WpfApplication/MessageTube.cs
@@ -19,8 +19,31 @@ namespace Abnaki.Windows.Software.Wpf
         public static void Subscribe<T>(Action<T> act)
         {
             var ps = agg.GetEvent<PubSubEvent<T>>();
-            ps.Subscribe(act);
+            ps.Subscribe(act, keepSubscriberReferenceAlive: true);
 
+        }
+
+        /// <summary>
+        /// Subscribe to subtype of published event
+        /// </summary>
+        /// <typeparam name="Tsuper">was associated with Publish</typeparam>
+        /// <typeparam name="Tsub">desired subtype</typeparam>
+        /// <param name="act"></param>
+        public static void Subscribe<Tsuper,Tsub>(Action<Tsub> act)
+            where Tsub : Tsuper
+        {
+            // Subscribe<Tsuper>(Demo<Tsuper>);
+
+            Subscribe<Tsuper>(super =>
+            {
+                if (super is Tsub)
+                    act((Tsub)super);
+            });
+        }
+
+        static void Demo<T>(T x)
+        {
+            object z = x;
         }
 
         static IEventAggregator agg = new EventAggregator();
