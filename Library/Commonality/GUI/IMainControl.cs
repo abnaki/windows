241dc9a527420412128555a5466052be561fabc4
abnaki <abnaki>
2016-05-20 17:51:48 -0400

    +ConfigureMenu()

--- Library/Commonality/GUI/IMainControl.cs
+++ Library/Commonality/GUI/IMainControl.cs
@@ -4,13 +4,16 @@ using System.Collections.Generic;
 namespace Abnaki.Windows.GUI
 {
     /// <summary>
-    /// Supports general initialization
+    /// Supports general initialization.
+    /// Expected to be implemented widely.
     /// </summary>
     public interface IMainControl
     {
         /// <summary>
-        /// Manages panels
+        /// Manages panels.  Can be null.
         /// </summary>
         IDockSystem DockingSystem { get; }
+
+        void ConfigureMenu(IMainMenu menu);
     }
 }
