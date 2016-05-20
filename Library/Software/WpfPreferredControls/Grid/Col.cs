de4deb7facfaf36769ea7721e1070a6fa85e3268
abnaki <abnaki>
2016-05-20 17:51:49 -0400

    +Col

---
+++ Library/Software/WpfPreferredControls/Grid/Col.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Abnaki.Windows.Software.Wpf.PreferredControls.Grid
+{
+    /// <summary>
+    /// Prescribes display of a column of a grid
+    /// </summary>
+    public class Col
+    {
+        public Col()
+        {
+
+        }
+
+        public Col(System.Data.DataColumn dcol)
+        {
+            this.Field = dcol.ColumnName;
+            this.Caption = dcol.Caption;
+        }
+
+        public string Field { get; set; }
+        public string Caption { get; set; }
+
+        // maybe Tooltip in the future
+    }
+}
