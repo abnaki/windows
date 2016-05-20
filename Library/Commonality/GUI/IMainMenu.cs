73e17b3162a03ce0f2dc0e4011f277d58503e217
abnaki <abnaki>
2016-05-20 17:51:48 -0400

    +IMainMenu and MenuSeed.  Shortened assembly name.

---
+++ Library/Commonality/GUI/IMainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Abnaki.Windows.GUI
+{
+    public interface IMainMenu
+    {
+        void AddCommand<Tkey>(MenuSeed<Tkey> seed);
+
+        void AddCommand<Tkey>(Tkey key, string label, bool? defaultCheck = null);
+
+        void AddCommandChild<Tkey>(object parentKey, Tkey childKey, string label, bool? defaultCheck = null);
+
+    }
+}
---
+++ Library/Commonality/GUI/MenuSeed.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Abnaki.Windows.GUI
+{
+    /// <summary>
+    /// Describes a menu command to create
+    /// </summary>
+    /// <typeparam name="Tkey"></typeparam>
+    public class MenuSeed<Tkey>
+    {
+        public MenuSeed()
+        {
+
+        }
+
+        public MenuSeed(Tkey key, string label, bool? defaultCheck = null, string toolTip = null)
+        {
+            this.Key = key;
+            this.Label = label;
+            this.DefaultCheck = defaultCheck;
+            this.Tooltip = toolTip;
+        }
+
+        public Tkey Key { get; set; }
+
+        /// <summary>
+        /// Can be null
+        /// </summary>
+        public object ParentKey { get; set; }
+
+        public string Label { get; set; }
+
+        /// <summary>
+        /// Null by default means no check exists.
+        /// </summary>
+        public bool? DefaultCheck { get; set; }
+
+        public string Tooltip { get; set; }
+
+        public bool? Enabled { get; set; }
+    }
+}
