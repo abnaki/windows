064fceb922d8937ef8c66adfcf89f336aa67926e
abnaki <abnaki>
2016-06-08 13:15:00 -0400

    RelativePath()

--- Library/Commonality/AbnakiFile.cs
+++ Library/Commonality/AbnakiFile.cs
@@ -36,20 +36,103 @@ namespace Abnaki.Windows
             return new DirectoryInfo(CombinedPath(parts));
         }
 
-        static string CombinedPath(IEnumerable parts)
+        static string CombinedPath(IEnumerable parts, bool allowRooting=true)
         {
             return parts.Cast<object>().Aggregate<object,string>("",
-                (a, b) => Path.Combine(a, FromGeneralizedPath(b)));
+                (a, b) => Path.Combine(a, FromGeneralizedPath(b, allowRooting && string.IsNullOrEmpty(a))));
         }
 
-        static string FromGeneralizedPath(object path)
+        static string FromGeneralizedPath(object path, bool shouldBeRooted)
         {
             if (path is FileSystemInfo)
-                return ((FileSystemInfo)path).FullName;
+            {
+                if (shouldBeRooted)
+                    return ((FileSystemInfo)path).FullName;
+                else
+                    return ((FileSystemInfo)path).Name;
+            }
             else if (path is string)
+            {
                 return Convert.ToString(path);
+            }
 
             throw new ArgumentException("Expected FileInfo or string but got " + path);
+        }
+
+        public static string RelativePath(FileSystemInfo fi, DirectoryInfo diRelated)
+        {
+            DirectoryInfo dibase = FindCommonDirectory(fi, diRelated);
+            if (dibase == null)
+                throw new ArgumentException("No common directory of " + fi.FullName + " and " + diRelated.FullName);
+
+            List<FileSystemInfo> paths = new List<FileSystemInfo>();
+
+            List<string> pathParts = new List<string>();
+            FindPathDifference(dibase, diRelated, paths); // may be nothing
+            string[] doubledots = Enumerable.Range(0, paths.Count).Select(i => "..").ToArray();
+            pathParts.Add(string.Join(Path.DirectorySeparatorChar.ToString(), doubledots));
+
+            paths.Clear();
+            FindPathDifference(dibase, fi, paths);
+            pathParts.Add(CombinedPath(paths, allowRooting: false));
+
+            if ( pathParts.Any(p => ! string.IsNullOrEmpty(p)) )
+                return CombinedPath(pathParts, allowRooting: false);
+
+            throw new ApplicationException("Failed in RelativePath of " + fi.FullName + " relative to " + diRelated.FullName);
+        }
+
+        static DirectoryInfo FindCommonDirectory(FileSystemInfo fi, DirectoryInfo di)
+        {
+            List<FileSystemInfo> fpaths = new List<FileSystemInfo>();
+            List<FileSystemInfo> dpaths = new List<FileSystemInfo>();
+            FindPathDifference(null, fi, fpaths);
+            FindPathDifference(null, di, dpaths);
+
+            DirectoryInfo dicommon = null;
+
+            for ( int i = 0; i < fpaths.Count && i < dpaths.Count ; i++ )
+            {
+                if (fpaths[i].FullName != dpaths[i].FullName)
+                    break;
+
+                dicommon = (DirectoryInfo)dpaths[i];
+            }
+
+            return dicommon;
+        }
+
+        /// <summary></summary>
+        /// <param name="dibase">null implies root</param>
+        /// <param name="fidescendant"></param>
+        /// <param name="paths"></param>
+        static void FindPathDifference(DirectoryInfo dibase, FileSystemInfo fidescendant, List<FileSystemInfo> paths)
+        {
+            if ( fidescendant == null )
+            {
+
+            }
+            else if ( dibase != null &&  fidescendant.FullName == dibase.FullName )
+            {
+
+            }
+            else
+            {
+                FileSystemInfo parent = GetParent(fidescendant);
+                FindPathDifference(dibase, parent, paths);
+
+                paths.Add(fidescendant);
+            }
+        }
+
+        static FileSystemInfo GetParent(FileSystemInfo fi)
+        {
+            if (fi is DirectoryInfo)
+                return ((DirectoryInfo)fi).Parent;
+            else if ( fi is FileInfo )
+                return ((FileInfo)fi).Directory;
+            else
+                throw new ArgumentException("GetParent cannot take " + fi);
         }
     }
 }
