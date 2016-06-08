0a5df10ba641436b1d3b63c1b2cd8ff0f646e711
abnaki <abnaki>
2016-06-08 13:14:59 -0400

    OK

---
+++ Testing/Commonality/CommonTest/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Abnaki.Windows;
+
+namespace CommonTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestRelativePath()
+        {
+            DirectoryInfo diCurrent = new DirectoryInfo(Environment.CurrentDirectory);
+            DirectoryInfo diGrandparent = diCurrent.Parent.Parent;
+
+            string rdir = AbnakiFile.RelativePath(diCurrent, diGrandparent);
+
+            Debug.WriteLine(rdir); // should be subdir such as bin/Debug
+
+            rdir = AbnakiFile.RelativePath(diGrandparent, diCurrent);
+
+            Debug.WriteLine(rdir); // should be ..\..
+
+        }
+    }
+}
