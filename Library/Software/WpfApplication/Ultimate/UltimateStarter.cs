9077ead2cc2509cc3b58077dacf879f6a4da741c
abnaki <abnaki>
2016-06-28 15:57:57 -0400

    Sets Icon

--- Library/Software/WpfApplication/Ultimate/UltimateStarter.cs
+++ Library/Software/WpfApplication/Ultimate/UltimateStarter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 
 namespace Abnaki.Windows.Software.Wpf.Ultimate
@@ -22,6 +23,12 @@ namespace Abnaki.Windows.Software.Wpf.Ultimate
 
             Tcontrol child = new Tcontrol();
             mw.SetMainControl(child);
+            
+            // Icon:  alphabetically first embedded resource .ico 
+            System.Reflection.Assembly a = child.GetType().Assembly;
+            string icname = a.GetManifestResourceNames().OrderBy(n => n).FirstOrDefault(n => n.EndsWith(".ico"));
+            if (icname != null)
+                mw.Icon = System.Windows.Media.Imaging.BitmapFrame.Create(a.GetManifestResourceStream(icname));
 
             return mw;
 
