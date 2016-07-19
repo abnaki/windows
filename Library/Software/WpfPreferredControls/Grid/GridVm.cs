015de08ef60a0c7e5913a8f17d0b4be86648efdf
abnaki <abnaki>
2016-07-19 10:50:23 -0400

    OK

---
+++ Library/Software/WpfPreferredControls/Grid/GridVm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Data;
+using PropertyChanged;
+
+namespace Abnaki.Windows.Software.Wpf.PreferredControls.Grid
+{
+    /// <summary>
+    /// ViewModel for Grid
+    /// </summary>
+    [ImplementPropertyChanged]
+    public class GridVm
+    {
+        public GridVm(IEnumerable data)
+        {
+            this.Data = new Xceed.Wpf.DataGrid.DataGridCollectionView(data);
+        }
+
+        public void Refresh()
+        {
+            this.Data.Refresh();
+        }
+
+        /// <summary>
+        /// Expect Grid to bind to it.
+        /// </summary>
+        public CollectionView Data {get; set;}
+    }
+}
