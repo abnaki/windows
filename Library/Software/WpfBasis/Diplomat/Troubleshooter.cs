be3a470d8ad114670121491f12369ef2ab89dac6
abnaki <abnaki>
2016-07-21 09:07:53 -0400

    Moved

---
+++ Library/Software/WpfBasis/Diplomat/Troubleshooter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Input;
+using SaveFileDialog = System.Windows.Forms.SaveFileDialog;
+
+namespace Abnaki.Windows.Software.Wpf.Diplomat
+{
+    public class Troubleshooter
+    {
+        public static bool DialogSaveLog()
+        {
+            bool saved = false;
+
+            using (SaveFileDialog dialog = new SaveFileDialog())
+            {
+                dialog.Filter = "XML|.xml";
+                dialog.DefaultExt = ".xml";
+                dialog.Title = "Save Log File";
+
+                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    using (new WaitCursor())
+                    {
+                        Abnaki.Windows.AbnakiLog.Write(dialog.FileName);
+                    }
+                    saved = true;
+                }
+            }
+            return saved;
+        }
+    }
+}
