6d41dea5ec4cf2ef39479f4c83937819ea5a668d
abnaki <abnaki>
2016-07-25 10:18:04 -0400

    OK

---
+++ Library/Software/WpfApplication/Ultimate/FarewellMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Abnaki.Windows.Software.Wpf.Ultimate
+{
+    /// <summary>
+    /// Occurs when application is shutting down
+    /// </summary>
+    public class FarewellMessage
+    {
+
+    }
+}
