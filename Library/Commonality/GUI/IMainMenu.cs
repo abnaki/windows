3495ec97457c7579e3ff8552fb210638d3053b76
abnaki <abnaki>
2016-07-25 10:17:00 -0400

    +Completed()

--- Library/Commonality/GUI/IMainMenu.cs
+++ Library/Commonality/GUI/IMainMenu.cs
@@ -13,5 +13,10 @@ namespace Abnaki.Windows.GUI
         void AddCommandChild<Tkey>(object parentKey, Tkey childKey, string label, bool? defaultCheck = null);
 
         void AddExclusiveCommands<TKey>(object parentKey, IEnumerable<MenuSeed<TKey>> seeds);
+
+        /// <summary>
+        /// All Commands added.  May restore saved Check property to commands.
+        /// </summary>
+        void Completed();
     }
 }
