7b409a66b104b29f1645b4ffbbaa88f3088a11ef
abnaki <abnaki>
2016-07-27 10:23:21 -0400

    Label and Detail

---
+++ Library/Commonality/LabelAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Abnaki.Windows
+{
+    /// <summary>
+    /// Labels and documents types, such as enum values
+    /// </summary>
+    public class LabelAttribute : Attribute
+    {
+        public LabelAttribute(string label)
+        {
+            this.Label = label;
+        }
+
+        /// <summary>
+        /// Shown on a control, button, and such things
+        /// </summary>
+        public string Label { get; set; }
+
+        /// <summary>
+        /// Exhaustive explanation
+        /// </summary>
+        public string Detail { get; set; }
+    }
+}
