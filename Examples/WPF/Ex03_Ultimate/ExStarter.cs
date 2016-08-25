c6cd88ed80607d1572971b7c6a19ba62185c6e95
abnaki <abnaki>
2016-08-25 15:45:48 -0400

    UpgradeUri

--- Examples/WPF/Ex03_Ultimate/ExStarter.cs
+++ Examples/WPF/Ex03_Ultimate/ExStarter.cs
@@ -10,6 +10,8 @@ namespace Ex03_Ultimate
         public static int Main(string[] args)
         {
             // Abnaki.Windows.Software.Wpf.Diplomat.Troubleshooter.Email = "nobody@nowhere.com";
+            Abnaki.Windows.Software.Wpf.Ultimate.MainMenuBus.UpgradeUri = new Uri("https://github.com/abnaki/windows/releases/latest");
+            // better to do:  Abnaki.Windows.Software.Wpf.Starter.CommonSettings(Properties.Settings.Default);
 
             return UltimateStarter<ExControl>.Start(args);
         }
