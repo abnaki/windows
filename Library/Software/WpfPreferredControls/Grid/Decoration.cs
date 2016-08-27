ec0f1003888b47c9498b2eb41ff7fa27ac743d8f
abnaki <abnaki>
2016-08-27 16:49:41 -0400

    +Decoration

---
+++ Library/Software/WpfPreferredControls/Grid/Decoration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Abnaki.Windows.Software.Wpf.PreferredControls.Grid
+{
+    class Decoration
+    {
+        public readonly Dictionary<string, string> MapColumnTooltip = new Dictionary<string, string>();
+
+        public string GetColumnTooltip(string fieldName)
+        {
+            if (MapColumnTooltip.ContainsKey(fieldName))
+                return MapColumnTooltip[fieldName];
+
+            return null;
+        }
+
+        public void Clear()
+        {
+            MapColumnTooltip.Clear();
+        }
+    }
+}
