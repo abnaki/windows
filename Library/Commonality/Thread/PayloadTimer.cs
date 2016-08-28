e7146b8f159fd4bf42a20d0d39f720dc42caca90
abnaki <abnaki>
2016-08-28 15:48:53 -0400

    +PayloadTimer

---
+++ Library/Commonality/Thread/PayloadTimer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace Abnaki.Windows.Thread
+{
+    /// <summary>
+    /// Raises Expire event once
+    /// </summary>
+    /// <typeparam name="T">of payload
+    /// </typeparam>
+    public class PayloadTimer<T> : IDisposable
+    {
+        public PayloadTimer(T payload, TimeSpan due)
+        {
+            //this.Payload = payload;
+            timer = new Timer(new TimerCallback(Callback), payload, (int)due.TotalMilliseconds, Timeout.Infinite);
+        }
+
+        //public T Payload { get; private set; }
+
+        public event Action<T> Expire;
+
+        System.Threading.Timer timer;
+
+        void Callback(object payload)
+        {
+            var h = Expire;
+            if (h != null)
+                h((T)payload);
+        }
+
+        public void Dispose()
+        {
+            if (timer != null)
+                timer.Dispose();
+
+            timer = null;
+        }
+    }
+}
