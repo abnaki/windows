a5bf4b74ae74798f9ccb984a77bb030edde1d2f4
abnaki <abnaki>
2016-08-29 16:41:27 -0400

    +Paneling.cs

---
+++ Library/Software/WpfPreferredControls/Docking/Paneling.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xceed.Wpf.AvalonDock;
+using Xceed.Wpf.AvalonDock.Layout;
+
+namespace Abnaki.Windows.Software.Wpf.PreferredControls.Docking
+{
+    public class Paneling
+    {
+        public static void ShowPanel(System.Windows.FrameworkElement control)
+        {
+            DockingManager dman = control.Parent as DockingManager;
+
+            if (dman != null)
+            {
+                LayoutAnchorable panel = dman.Layout.Descendents().OfType<LayoutAnchorable>()
+                    .FirstOrDefault<LayoutAnchorable>(lay => lay.Content == control);
+
+                if (panel != null && panel.IsAutoHidden )
+                {
+                    panel.ToggleAutoHide();
+                }
+            }
+
+        }
+    }
+}
