cef75ab067f59fcda13e1cb6058087397753de7a
abnaki <abnaki>
2016-09-07 11:20:27 -0400

    AppendMessage() avoids redundant messages

--- Library/Software/WpfBasis/Diplomat/Notifier.cs
+++ Library/Software/WpfBasis/Diplomat/Notifier.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Windows;
+using System.Linq;
+using System.Diagnostics;
 
 namespace Abnaki.Windows.Software.Wpf.Diplomat
 {
@@ -21,6 +23,8 @@ namespace Abnaki.Windows.Software.Wpf.Diplomat
             // wish to prompt to use Troubleshooter for situation when no menu is available.
         }
 
+        static Queue<string> recentErrors = new Queue<string>();
+
         /// <summary>
         /// Log and show to user
         /// </summary>
@@ -28,8 +32,17 @@ namespace Abnaki.Windows.Software.Wpf.Diplomat
         {
             Abnaki.Windows.AbnakiLog.Exception(ex);
 
-            Error(CumulativeMessage(ex));
+            string msg = CumulativeMessage(ex);
 
+            if (AppendMessage(msg))
+            {
+                Error(msg);
+            }
+            else
+            {
+                // avoid plaguing UI with redundant messages
+                Debug.WriteLine("Skipped UI notification of " + msg);
+            }
         }
 
         static string CumulativeMessage(Exception ex)
@@ -38,6 +51,27 @@ namespace Abnaki.Windows.Software.Wpf.Diplomat
                 return ex.Message;
             else
                 return ex.Message + Environment.NewLine + CumulativeMessage(ex.InnerException);
+        }
+
+        /// <returns>true if errorMsg is different from recent messages
+        /// </returns>
+        static bool AppendMessage(string errorMsg)
+        {
+            lock (recentErrors)
+            {
+                int limit = 4;
+                if (recentErrors.Count >= limit && recentErrors.All(m => m == errorMsg))
+                {
+                    return false;
+                }
+                while (  recentErrors.Count >= limit )
+                {
+                    recentErrors.Dequeue();
+                }
+
+                recentErrors.Enqueue(errorMsg);
+                return true;
+            }
         }
     }
 }
