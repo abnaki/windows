c6f116143a18f5304303e554c98988723a93e527
abnaki <abnaki>
2016-09-14 11:47:54 -0400

    Compare class

---
+++ Library/Commonality/Compare.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Abnaki.Windows
+{
+    /// <summary>
+    /// Supports IComparables
+    /// </summary>
+    public static class Compare
+    {
+        /// <summary>
+        /// Uses CompareTo(), defines null as less than non-null, and null equal to null.
+        /// </summary>
+        /// <typeparam name="T">IComparable of T</typeparam>
+        /// <param name="thing"></param>
+        /// <param name="other"></param>
+        public static int CompareNullPossible<T>(T thing, T other)
+            where T : IComparable<T>
+        {
+            if (thing == null)
+            {
+                return other == null ? 0 : 1;
+            }
+            else if ( other == null )
+            {
+                return -1;
+            }
+
+            return thing.CompareTo(other);
+        }
+    }
+}
