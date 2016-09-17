fc79f9139c4eed7677be6f52e6a5915588139b16
abnaki <abnaki>
2016-09-17 11:43:54 -0400

    +RecordCellEventArgs

---
+++ Library/Software/WpfPreferredControls/Grid/Event/RecordCellEventArgs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Abnaki.Windows.Software.Wpf.PreferredControls.Grid.Event
+{
+    /// <summary>
+    /// Cell of a Field within a record or row
+    /// </summary>
+    public class RecordCellEventArgs
+    {
+        public Xceed.Wpf.DataGrid.DataGridItemEventArgs ItemArgs { get; set; }
+        
+        /// <summary>
+        /// Name of property of Record
+        /// </summary>
+        public string Field { get; set; }
+
+        /// <summary>
+        /// Preferred
+        /// </summary>
+        public object Record
+        {
+            get
+            {
+                return ItemArgs.Item;
+            }
+        }
+    }
+}
