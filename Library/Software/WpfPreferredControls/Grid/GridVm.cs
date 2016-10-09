859092b618cfb4a17b471edf654cc52546d5e96b
abnaki <abnaki>
2016-10-09 15:18:49 -0400

    AddRecords and DeleteRecords commands

--- Library/Software/WpfPreferredControls/Grid/GridVm.cs
+++ Library/Software/WpfPreferredControls/Grid/GridVm.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Data;
+using System.Windows;
+using System.Windows.Input;
 using PropertyChanged;
 
 namespace Abnaki.Windows.Software.Wpf.PreferredControls.Grid
@@ -16,6 +18,9 @@ namespace Abnaki.Windows.Software.Wpf.PreferredControls.Grid
         public GridVm(IEnumerable data)
         {
             this.Data = new Xceed.Wpf.DataGrid.DataGridCollectionView(data);
+
+            this.AddRecords = new Command.CmdAddRecords(this.Data);
+            this.DeleteRecords = new Command.CmdDeleteRecords(this.Data);
         }
 
         public void Refresh()
@@ -28,5 +33,8 @@ namespace Abnaki.Windows.Software.Wpf.PreferredControls.Grid
         /// Inherits System.Windows.Data.CollectionView.
         /// </summary>
         public Xceed.Wpf.DataGrid.DataGridCollectionView Data { get; set; }
+
+        public ICommand AddRecords { get; set; }
+        public ICommand DeleteRecords { get; set; }
     }
 }
