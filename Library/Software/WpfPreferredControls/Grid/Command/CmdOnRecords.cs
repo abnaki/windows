370db6bba7c83ebb8f4715f5914afc500a7634fa
abnaki <abnaki>
2016-10-09 15:18:50 -0400

    OK

---
+++ Library/Software/WpfPreferredControls/Grid/Command/CmdOnRecords.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Windows.Input;
+
+using Xceed.Wpf.DataGrid;
+
+namespace Abnaki.Windows.Software.Wpf.PreferredControls.Grid.Command
+{
+    abstract class CmdOnRecords : ICommand
+    {
+        public CmdOnRecords(DataGridCollectionView dcv)
+        {
+            this.Collection = dcv;
+
+            this.Table = this.Collection.SourceCollection as DataTable;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return this.Collection.SourceCollection != null;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public abstract void Execute(object parameter);
+
+        protected DataGridCollectionView Collection { get; private set; }
+
+        protected DataTable Table { get; private set; }
+
+
+    }
+}
