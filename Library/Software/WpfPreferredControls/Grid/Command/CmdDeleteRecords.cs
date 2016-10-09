5aa8d14929b200b474938477e2491b9ceeb7a435
abnaki <abnaki>
2016-10-09 15:18:50 -0400

    OK

---
+++ Library/Software/WpfPreferredControls/Grid/Command/CmdDeleteRecords.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+
+
+namespace Abnaki.Windows.Software.Wpf.PreferredControls.Grid.Command
+{
+    class CmdDeleteRecords : CmdOnRecords
+    {
+        public CmdDeleteRecords(Xceed.Wpf.DataGrid.DataGridCollectionView dcv) : base(dcv)
+        {
+        }
+
+        public override void Execute(object parameter)
+        {
+            // DataGridCollectionView does not have any more general valid Remove functionality
+
+            if (this.Table != null)
+            {
+                DataRow dr = this.Collection.CurrentItem as DataRow;
+                if (dr != null)
+                {
+                    this.Table.Rows.Remove(dr);
+                    this.Collection.Refresh();
+                }
+            }
+        }
+    }
+}
