6a5a7e2a001259dda66f7019231ac92de0221502
abnaki <abnaki>
2016-11-04 09:44:53 -0400

    +RichTag, ICommands, Window.InputBindings, -NHotkey

--- Library/Software/WpfApplication/Menu/MainMenu.xaml.cs
+++ Library/Software/WpfApplication/Menu/MainMenu.xaml.cs
@@ -17,6 +17,7 @@ using System.Windows.Shapes;
 using Abnaki.Windows.GUI;
 using Abnaki.Windows.Software.Wpf.Ultimate;
 using Abnaki.Windows.Software.Wpf.Profile;
+using System.Diagnostics;
 
 namespace Abnaki.Windows.Software.Wpf.Menu
 {
@@ -85,31 +86,24 @@ namespace Abnaki.Windows.Software.Wpf.Menu
 
         void CompleteItem<Tkey>(MenuItem item, MenuSeed<Tkey> seed)
         {
-            RoutedEventHandler handler = null;
-            if (seed.MutuallyExclusive)
-            {
-                handler = ItemClickExclusive<Tkey>;
-            }
-            else
-            {
-                handler = ItemClick<Tkey>; // usual
-            }
-
-            item.Click += handler;
+            RichTag rt = (RichTag)item.Tag;
+            ICommand command = seed.MutuallyExclusive ? new ClickCommandExclusive<Tkey>() : new ClickCommand<Tkey>();
+            item.Command = command;
+            item.CommandParameter = item.Tag;
 
             if ( seed.DefaultCheck.HasValue )
             {
                 mapKeyExclusivity[seed.Key] = seed.MutuallyExclusive;
             }
 
-            string tip = seed.Tooltip ?? AbnakiReflection.DetailOfEnum(item.Tag);
+            string tip = seed.Tooltip ?? rt.LabelDetail;
             //  maybe wrap
             //  Regex rgx = new Regex("(.{50}\\s)"); string s = rgx.Replace(longMessage,"$1\n"); 
 
             if ( seed.ShortcutKey != Key.None )
             {
-                NHotkey.Wpf.HotkeyManager.Current.AddOrReplace(seed.Key.ToString(), seed.ShortcutKey, seed.ShortcutModifier,
-                    (sender, e) => HandleShortcutKey(handler, item, sender, e));
+                KeyGesture keyg = new KeyGesture(seed.ShortcutKey, seed.ShortcutModifier);
+                Application.Current.MainWindow.InputBindings.Add(new InputBinding(item.Command, keyg) { CommandParameter = item.CommandParameter });
 
                 if (!string.IsNullOrWhiteSpace(seed.Tooltip))
                     tip += " ";
@@ -125,41 +119,100 @@ namespace Abnaki.Windows.Software.Wpf.Menu
             if ( seed.Enabled.HasValue )
                 item.IsEnabled = seed.Enabled.Value;
 
+            CompleteParentMenuItem(item);
+        }
+
+        static void CompleteParentMenuItem(MenuItem item)
+        {
+            // expects child items to have Tag using RichTag because it is IComparable
             item.Items.SortDescriptions.Clear();
-            item.Items.SortDescriptions.Add(new System.ComponentModel.SortDescription("CommandParameter", System.ComponentModel.ListSortDirection.Ascending));
+            item.Items.SortDescriptions.Add(new System.ComponentModel.SortDescription("Tag", System.ComponentModel.ListSortDirection.Ascending));
         }
 
-        static void ItemClick<Tkey>(object sender, RoutedEventArgs e)
+        class RichTag : IComparable<RichTag>, IComparable
         {
-            MenuItem item = (MenuItem)sender;
-            Tkey key = (Tkey)item.Tag;
-            bool? isChecked = item.IsCheckable ? (bool?)item.IsChecked : (bool?)null;
-
-            ButtonBus<Tkey>.Handle(key, isChecked); // the whole point
-
-            e.Handled = true;  // otherwise, tunnels down through MenuItem hierarchy
-            
-        }
-
-        static void ItemClickExclusive<Tkey>(object sender, RoutedEventArgs e)
-        {
-            ItemClick<Tkey>(sender, e);
-
-            // turn off sibiling items
-            MenuItem item = (MenuItem)sender;
-            MenuItem parent = (MenuItem)item.Parent;
-            foreach ( MenuItem otherItem in  parent.Items )
+            public RichTag(MenuItem item, object tag)
             {
-                if (otherItem != item)
-                    otherItem.IsChecked = false;
+                this.Item = item;
+                this.Tag = tag;
+            }
+
+            public MenuItem Item { get; set; }
+            
+            /// <summary>
+            /// Enum value
+            /// </summary>
+            public object Tag { get; set; }
+
+            public string Qualifier
+            {
+                get { return this.Tag.GetType().FullName + "." + this.Tag; }
+            }
+
+            public string LabelDetail
+            {
+                get { return AbnakiReflection.DetailOfEnum(this.Tag); }
+            }
+
+            /// <summary>
+            /// required for disparate enums via SortDescriptions
+            /// </summary>
+            int TagInteger { get { return (int)this.Tag; } }
+
+            public int CompareTo(RichTag other)
+            {
+                return this.TagInteger.CompareTo(other.TagInteger);
+            }
+
+            int IComparable.CompareTo(object other)
+            {
+                return CompareTo((RichTag)other);
             }
         }
 
-        static void HandleShortcutKey(RoutedEventHandler h, MenuItem item, object sender, NHotkey.HotkeyEventArgs e)
+        class ClickCommand<Tkey> : ICommand
         {
-            if (item.IsEnabled)
+            public bool CanExecute(object parameter)
             {
-                h(item, new RoutedEventArgs(MenuItem.ClickEvent));
+                return true;
+            }
+
+            public event EventHandler CanExecuteChanged;
+
+            public virtual void Execute(object parameter)
+            {
+                RichTag rt = (RichTag)parameter;
+                MenuItem item = (MenuItem)rt.Item;
+                if (false == item.IsEnabled)
+                    return;
+
+                Tkey key = (Tkey)rt.Tag;
+                bool? isChecked = item.IsCheckable ? (bool?)item.IsChecked : (bool?)null;
+
+                ButtonBus<Tkey>.Handle(key, isChecked); // the whole point
+
+                //e.Handled = true;  // otherwise, Click event tunneled down through MenuItem hierarchy
+            }
+        }
+
+        class ClickCommandExclusive<Tkey> : ClickCommand<Tkey>
+        {
+            public override void Execute(object parameter)
+            {
+                base.Execute(parameter);
+
+                // turn off sibiling items
+                RichTag rt = (RichTag)parameter;
+                MenuItem item = (MenuItem)rt.Item;
+                if (false == item.IsEnabled)
+                    return;
+
+                MenuItem parent = (MenuItem)item.Parent;
+                foreach (MenuItem otherItem in parent.Items)
+                {
+                    if (otherItem != item)
+                        otherItem.IsChecked = false;
+                }
             }
         }
 
@@ -200,7 +253,7 @@ namespace Abnaki.Windows.Software.Wpf.Menu
         {
             foreach (string qualifier in qualifiers)
             {
-                MenuItem item = FindItem(this.RootMenu.Items, tag => qualifier == MenuCheckPreference.QualifierOfTag(tag));
+                MenuItem item = FindItem(this.RootMenu.Items, tag => qualifier == tag.Qualifier);
                 if (item != null)
                 {
                     item.IsChecked = chk;
@@ -211,12 +264,12 @@ namespace Abnaki.Windows.Software.Wpf.Menu
 
         MenuItem FindItem(ItemCollection items, object tag)
         {
-            return FindItem(items, othertag => tag.Equals(othertag));
+            return FindItem(items, othertag => othertag.Tag.Equals(tag));
         }
 
-        MenuItem FindItem(ItemCollection items, Func<object, bool> testTag)
+        MenuItem FindItem(ItemCollection items, Func<RichTag, bool> testTag)
         {
-            MenuItem foundItem = items.Cast<MenuItem>().FirstOrDefault(item => testTag(item.Tag));
+            MenuItem foundItem = items.Cast<MenuItem>().FirstOrDefault(item => testTag(item.Tag as RichTag));
             if (foundItem == null )
             { // recurse
                 foreach ( MenuItem item in items )
@@ -231,7 +284,8 @@ namespace Abnaki.Windows.Software.Wpf.Menu
 
         static void RaiseExistingCheck(MenuItem item)
         {
-            item.RaiseEvent(new RoutedEventArgs(MenuItem.ClickEvent));
+            //item.RaiseEvent(new RoutedEventArgs(MenuItem.ClickEvent));
+            item.Command.Execute(item.CommandParameter);
         }
 
         static void RaiseExistingChecks(ItemCollection items)
@@ -252,9 +306,8 @@ namespace Abnaki.Windows.Software.Wpf.Menu
 
             MenuItem item = new MenuItem();
             item.Header = label;
-            //AssignTag(item, key);
-            item.Tag = key;
-            AssignIntegerSequence(item, key);
+            
+            item.Tag = new RichTag(item, key);
 
             item.IsCheckable = defaultCheck.HasValue;
             if (defaultCheck.HasValue)
@@ -268,13 +321,6 @@ namespace Abnaki.Windows.Software.Wpf.Menu
             }
 
             return item;
-        }
-
-        static void AssignIntegerSequence(MenuItem item, object key)
-        {
-            // Nothing special about CommandParameter; it is an available object field.
-            // (int) required for disparate enums via SortDescriptions.
-            item.CommandParameter = (int)key; 
         }
 
         void Farewell(FarewellMessage msg)
@@ -309,19 +355,20 @@ namespace Abnaki.Windows.Software.Wpf.Menu
                 Descend(items, allowUncheck);
             }
 
-            void Descend(ItemCollection items, Func<object, bool> allowUncheck)
+            void Descend(ItemCollection items, Func<RichTag, bool> allowUncheck)
             {
                 foreach (MenuItem item in items)
                 {
-                    if (item.Tag != null)
+                    RichTag rt = item.Tag as RichTag;
+                    if (rt != null)
                     {
-                        string qualifier = QualifierOfTag(item.Tag);
+                        string qualifier = rt.Qualifier;
 
                         if (item.IsChecked)
                         {
                             CheckedEnums.Add(qualifier);
                         }
-                        else if (allowUncheck(item.Tag))
+                        else if (allowUncheck(rt))
                         {
                             UncheckedEnums.Add(qualifier);
                         }
@@ -330,11 +377,6 @@ namespace Abnaki.Windows.Software.Wpf.Menu
                 }
             }
 
-            /// <param name="tag">enum</param>
-            public static string QualifierOfTag(object tag) 
-            {
-                return tag.GetType().FullName + "." + tag;
-            }
         }
 
     }
