455f0ec0f0fe39f8d91527ac6582c000e3e9e2c1
abnaki <abnaki>
2016-11-28 14:10:19 -0500

    +Dragging.cs

---
+++ Library/Software/WpfBasis/NetAid/Dragging.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+namespace Abnaki.Windows.Software.Wpf.NetAid
+{
+    /// <summary>
+    /// Assists with Drag/Drop
+    /// </summary>
+    public class Dragging
+    {
+        /// <summary>
+        /// Data that was passed to System.Windows.DragDrop.DoDragDrop()
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="e"></param>
+        public static T Extract<T>(DragEventArgs e) where T : class
+        {
+            return e.Data.GetData(typeof(T)) as T;
+        }
+    }
+}
