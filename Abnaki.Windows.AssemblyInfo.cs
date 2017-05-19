2c93e9261e1d00b1ae2d93646d0b5c3937025989
abnaki <abnaki>
2017-05-19 11:45:00 -0400

    2.0.68.0

--- Abnaki.Windows.AssemblyInfo.cs
+++ Abnaki.Windows.AssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Reflection;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("2.0.66.0")]
+[assembly: AssemblyVersion("2.0.68.0")]
 //[assembly: AssemblyFileVersion("     ")] // respects AssemblyVersion
